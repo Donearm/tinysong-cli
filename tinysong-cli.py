32beb6339341c7c5fc091b111fb0658cf4f7a91b
Gianluca Fiore <forod.g@gmail.com>
2012-05-30 09:20:06 +0200

    switch to argparse and some code cleaning

--- tinysong-cli.py
+++ tinysong-cli.py
@@ -24,7 +24,7 @@ import urllib2
 import json
 import tweepy
 from urllib import urlencode
-from optparse import OptionParser
+from argparse import ArgumentParser
 from browser import open_url_in_browser
 from tinysong_twitter import tw_authenticate, tw_tweet_song
 from tinysong_mpd import mpd_get_song
@@ -46,36 +46,40 @@ except ImportError:
 
 def argument_parser():
     usage = "usage: pytinysong.py [options] searchterm(s)"
-    arguments = OptionParser(usage=usage, version="%prog 0.1")
-    arguments.add_option("-b", "--metasearch",
+    parser = ArgumentParser(usage=usage, version="%prog 0.1")
+    parser.add_argument("-b", "--metasearch",
             help="do a metasearch (returns back more info about the song)",
             action="store_true",
             dest="metasearch")
-    arguments.add_option("-l", "--limit",
+    parser.add_argument("-l", "--limit",
             # API limit should be 32 but apparently it stops returning results 
             # at 14
             help="the limit to the number of results, between 1 and 14",
             action="store",
             dest="limit")
-    arguments.add_option("-o", "--open-url",
+    parser.add_argument("-o", "--open-url",
             help="open the url in browser.\nUrl will be the only or the first one (in case of multiple results)",
             action="store_true",
             dest="openbrowser")
-    arguments.add_option("-t", "--tweet",
+    parser.add_argument("-t", "--tweet",
             help="tweet the first song found on Twitter",
             action="store_true",
             dest="tweet")
-    arguments.add_option("-m", "--mpd",
+    parser.add_argument("-m", "--mpd",
             help="get current playing song from a MPD server",
             action="store_true",
             dest="mpd")
-    arguments.add_option("-c", "--cmus",
+    parser.add_argument("-c", "--cmus",
             help="get current playing song from Cmus",
             action="store_true",
             dest="cmus")
+    parser.add_argument(action="store",
+            help="query parameters for tinysong",
+            nargs="*",
+            dest="args")
 
-    (options, args) = arguments.parse_args()
-    return options, args
+    options = parser.parse_args()
+    return options
 
 def tinysong_search(url):
     """make a search on tinysong"""
@@ -167,9 +171,9 @@ class TinySongSearcher():
 
 
 def main():
-    options, args = argument_parser()
+    options = argument_parser()
 
-    joined_args = '+'.join(args)
+    joined_args = '+'.join(options.args)
 
     ts = TinySongSearcher()
 
@@ -187,32 +191,10 @@ def main():
 
     if options.metasearch:
         result_url, artistname, albumname, songname = ts.meta_search(joined_args)
-#        url = BASEURL + '/b/' + joined_args + '?format=json&key=' + APIKEY
-#        result = tinysong_search(url)
-#        print(result)
-#        result_url = result['Url']
-#        print("%s - %s - %s # %s" % (result['ArtistName'], result['AlbumName'], result['SongName'], result_url[0]))
     elif options.limit:
         result_url, artistname, albumname, songname = ts.limit_search(joined_args, options.limit)
-#        url = BASEURL + '/s/' + joined_args + '?format=json&limit=' + str(options.limit) + '&key=' + APIKEY
-#        result = tinysong_search(url)
-#        print(result)
-#        try:
-#            result_url = result[0]['Url']
-#        except (TypeError, IndexError):
-#            not_found()
-#        for n in range(0, int(options.limit)):
-#            try:
-#                print("%s - %s - %s # %s" % (result[n]['ArtistName'], result[n]['AlbumName'], result[n]['SongName'], result[n]['Url']))
-#            except IndexError:
-#                pass
     else:
-#        result_url, artistname, albumname, songname = ts.basic_search(joined_args)
         result_url = ts.basic_search(joined_args)
-#        url = BASEURL + '/a/' + joined_args + '?format=json&key=' + APIKEY
-#        result = tinysong_search(url)
-#        result_url = result
-#        print(result_url)
 
     # stop if we haven't got any result from tinysong
     if not result_url:
