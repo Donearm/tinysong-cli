c2ae97cc874b5e38e331b8d368bb03e66f48472e
Gianluca Fiore <forod.g@gmail.com>
2012-06-13 15:10:50 +0200

    default config file added

---
+++ tinysongconfig-default.py
@@ -0,0 +1,14 @@
+#-*- coding: utf-8 -*-
+
+APIKEY = ''
+TW_CONSUMER = 'oLbNuUAAKJtOtUgNvEdTg'
+TW_CONSUMER_SECRET = 'EgR8h2qv5pSWw1QSFbdJpaMMcjrPK6CZNJrYK14c'
+
+# Format of the tweet: 
+# PREP is what comes before the song link, POSTP is what comes after
+# Leave empty either (or even both) if you want the tinysong link to be
+# at the beginning/end of the tweet (or if you just don't care to add hashtags
+# and/or custom strings).
+# Don't use too many characters or you'll risk hitting the 140 chars limit.
+PREP = ''
+POSTP = ''
